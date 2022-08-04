27f06cccb1f3abd197b88d2cb91e9cd4c14e3694
Glauco Dante <108493608+glaucodante@users.noreply.github.com>
2022-08-04 19:37:43 -0300

    Finalizando as laterações no Projeto Urna

---
+++ Projetos/ProjetoUrna/myUrna/etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano Safado',
+                partido: 'PQP',
+                fotos: [
+                    { url: '38111.jpg', legenda: 'Vereador', small: false }
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano sem Futuro',
+                partido: 'DEFG',
+                fotos: [
+                    { url: '77222.jpg', legenda: 'Vereador', small: false }
+                ]
+            },
+        ]
+
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano sem Vergonha',
+                partido: 'PQP',
+                vice: 'Cic',
+                fotos: [
+                    { url: '99.jpg', legenda: 'Prefeito' },
+                    { url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano Nó Cego',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    { url: '84.jpg', legenda: 'Prefeito' },
+                    { url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+];
